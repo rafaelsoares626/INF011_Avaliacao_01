f259dd3c3b61e6b1f27daf817dc573dc397aa8e6
silviocdjm <81332335+silviocdjm@users.noreply.github.com>
2021-11-10 22:03:44 -0300

    Update TipoProduto.java

---
+++ src/TipoProduto.java
@@ -0,0 +1,18 @@
+package Model;
+
+public enum TipoProduto {
+
+	LIVRO("Model.LivroFactory"),
+	DISCIPLINA("Model.DisciplinaFactory");
+	
+	String className;
+	
+	private TipoProduto(String className) {
+		this.className = className;
+	}
+	
+	public String getFactoryName() {
+		return this.className;
+	}
+	
+}
